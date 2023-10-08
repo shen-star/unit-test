f2fd307951d6c0c93e497ebd6d037b8b8403388e
Alex <1172068437@qq.com>
2023-10-08 09:41:05 +0800

    feat:add scaffold

---
+++ src/main/java/com/shen/inspection/InspectionApplication.java
@@ -0,0 +1,16 @@
+package com.shen.inspection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class InspectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shen/inspection/other/DemoTestOne.java
@@ -0,0 +1,14 @@
+package com.shen.inspection.other;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class DemoTestOne {
+    @Test
+    void testOne() {
+        log.info("this is test one");
+        Assertions.assertTrue(true);
+    }
+}
