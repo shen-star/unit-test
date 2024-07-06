c6ffe7f4d199209b6a1f1b37a6147cf7347ccf67
Alex <1172068437@qq.com>
2024-07-06 21:53:55 +0800

    feat:add socket test remarks V0.0.1

--- src/main/java/com/shen/inspection/modules/socket/SocketTcp01Client.java
+++ src/main/java/com/shen/inspection/modules/socket/SocketTcp01Client.java
@@ -3,19 +3,33 @@ package com.shen.inspection.modules.socket;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
 /**
- * TCP网络编程示例-服务端
+ * TCP网络编程示例-客户端
  */
 @Slf4j
 public class SocketTcp01Client {
     public static void main(String[] args) throws IOException {
+        //连接服务端(ip,端口号)，连接上服务端后生成socket
         Socket socket = new Socket(InetAddress.getLocalHost(),18099);
+        //通过输出流，写入数据到数据通道
         OutputStream outputStream = socket.getOutputStream();
         outputStream.write("hello server".getBytes());
+        //设置结束标志
+        socket.shutdownOutput();
+        //通过getInputStream()方法，读取服务端写入到数据通道的数据
+        InputStream inputStream = socket.getInputStream();
+        byte[] bytes = new byte[1024];
+        int len;
+        while ((len = inputStream.read(bytes)) != -1) {
+            log.info(new String(bytes, 0, len));
+        }
+        //关闭流和socket
+        inputStream.close();
         outputStream.close();
         socket.close();
         log.info("客户端 结束");
--- src/main/java/com/shen/inspection/modules/socket/SocketTcp01Server.java
+++ src/main/java/com/shen/inspection/modules/socket/SocketTcp01Server.java
@@ -4,24 +4,35 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- * TCP网络编程示例-客户端
+ * TCP网络编程示例-服务端
  */
 @Slf4j
 public class SocketTcp01Server {
     public static void main(String[] args) throws IOException {
+        //在本机监听18089端口，等待连接(无连接时阻塞)
         ServerSocket serverSocket = new ServerSocket(18099);
         log.info("正在监听 18099 端口号");
+        //serverSocket可以通过accept()方法返回多个Socket
         Socket socket = serverSocket.accept();
+        //通过getInputStream()方法，读取客户端写入到数据通道的数据
         InputStream inputStream = socket.getInputStream();
         byte[] bytes = new byte[1024];
         int len;
         while ((len = inputStream.read(bytes)) != -1) {
             log.info(new String(bytes, 0, len));
         }
+        //通过getOutputStream()方法，将数据写入数据通道
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write("hello client".getBytes());
+        //写入后设置结束标志
+        socket.shutdownOutput();
+        //关闭流和socket
+        outputStream.close();
         inputStream.close();
         socket.close();
         serverSocket.close();
